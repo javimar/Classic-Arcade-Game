f3d53167d749b01172ff9f6efb28cf3c85aa5b69
javimar <javimardeveloper@gmail.com>
2018-04-01 19:11:33 +0200

    Add enemy logic

--- js/app.js
+++ js/app.js
@@ -5,13 +5,16 @@ const canvasWidth = 505;
 const cellWidth = 101; // width of png images
 const cellHeight = 82; // visible height of png
 
+const spriteHeight = 70; // height of image
+
 // Enemies our player must avoid
 class Enemy
 {
-  constructor(x, y)
+  constructor(x, y, speed)
   {
     this.x = x;
     this.y = y;
+    this.speed = speed;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -24,7 +27,24 @@ class Enemy
       // You should multiply any movement by the dt parameter
       // which will ensure the game runs at the same speed for
       // all computers.
+      this.x = this.x + (this.speed * dt); // move from left to right at specified speed
 
+      // when bugs move out of canvas, place them way back at the beginning
+      // and update their speed
+      if(this.x > canvasWidth)
+      {
+          this.x = -100;
+          this.speed = (Math.random() * 120) + 100;
+      }
+
+      // handle collision with Player
+      if(player.x < this.x + 60 &&
+          player.x + 60 > this.x &&
+          player.y < this.y + spriteHeight &&
+      	  player.y + spriteHeight > this.y)
+      {
+          player.reset();
+      }
   }
 
   // Draw the enemy on the screen, required method for game
@@ -88,14 +108,14 @@ class Player
           this.y = 400;
       }
 
-      // reaching the water will place player back to starting position after 3ms
+      // reaching the water will place player back at starting position after 3ms
       if(this.y < 1)
       {
-          this.resetPlayer();
+          this.reset();
       }
   }
 
-  resetPlayer()
+  reset()
   {
     setTimeout(function()
     {
@@ -107,12 +127,21 @@ class Player
 } // end of class Player
 
 // Instantiate the objects
+const rows = [
+    spriteHeight, spriteHeight+cellHeight, spriteHeight+cellHeight*2,
+    spriteHeight, spriteHeight+cellHeight, spriteHeight+cellHeight*2,
+    spriteHeight, spriteHeight+cellHeight, spriteHeight+cellHeight*2];
+const enemySpeed = (Math.random() * 200) + 50;
 
-// Place all enemy objects in an array called allEnemies
+rows.forEach(function (yCoord)
+{
+	enemy = new Enemy(0, yCoord, enemySpeed);
+    // Place all enemy objects in an array called allEnemies
+	allEnemies.push(enemy);
+});
 
 // new player initial position based on image and canvas size
 const player = new Player(cellWidth * 2, 400);
-
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
